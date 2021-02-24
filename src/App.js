abe1435efefb2e6fa547bec2c1390a5474264c8a
ivan-user <username.web@protonmail.com>
2021-02-24 12:25:07 +0100

    Done with help

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import React, { Component } from "react";
+import contacts from './contacts.json';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+    state = {
+         contacts : contacts.slice(0,5),
+         allContacts: contacts.slice(5, contacts.length)
+    }
+
+     addRandomContact = () => {
+        const contactsCopy = [...this.state.allContacts];
+        const randomIndex = Math.floor(Math.random() * contacts.length);
+        const randomContact = contactsCopy.splice(randomIndex, 1);
+        const newArr = [...this.state.contacts]
+        newArr.push(randomContact[0]);
+        this.setState({ contacts: newArr, allContacts: contactsCopy })
+    }
+
+    sortNames = () => {
+        const newArr = [...this.state.contacts]
+
+        newArr.sort( (a,b) => {
+            if(a.name < b.name) return -1
+            if(a.name > b.name) return 1
+            else return 0
+        })
+        this.setState({ contacts: newArr })
+    }
+
+    sortPopularity = () => {
+        const newSortedArr = [...this.state.contacts]
+        newSortedArr.sort((a,b) => b.popularity - a.popularity)
+        this.setState({ contacts: newSortedArr })
+    }
+
+    deleteContact = (index) => {
+        const newSortedArr = [...this.state.contacts]
+        const filteredArr = newSortedArr.filter((el, i, arr) => (arr[i].id !== arr[index].id))
+        this.setState({ contact: filteredArr })
+    }
+
+  render (){
+        return (
+            <div className="App">
+
+                <h1>Ironhack Contacts</h1>
+
+                <button onClick={ this.addRandomContact } className='btn' >Add random contact</button>
+                <button onClick={ this.sortNames } className='btn' >Sort by name</button>
+                <button onClick={ this.sortPopularity } className='btn' >Sort by popularity</button>
+
+                <table className='table'>
+                    <thead>
+                        <tr>
+                            <th>Picture</th>
+                            <th>Name</th>
+                            <th>Popularity</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    {this.state.contacts.map((contact, index) => {
+                        return (
+                            <tr key={contact.id}>
+                                <td><img src={contact.pictureUrl} style={{height:'200px'}} alt='profilePicture' /></td>
+                                <td>{contact.name}</td>
+                                <td>{contact.popularity.toFixed(1)}</td>
+                                <td> <button className='btn' onClick = { ()=> { this.deleteContact(index) }}>Delete</button></td>
+                            </tr>
+                        )
+                    })}
+                        
+                    </tbody>
+                </table>
+            </div>
+        )
+    }
 }
 
 export default App;
